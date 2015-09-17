6c5bcfce86e744099abac17b5d824262005bf5af
Patrick Allen <prallen90@gmail.com>
2015-09-16 22:40:25 -0400

    Add get_results method to AsyncRequestHandler

--- main.py
+++ main.py
@@ -2,9 +2,7 @@ import csv
 import asyncio
 import aiohttp
 import datetime
-
-CONCURRENT_REQUESTS = 15
-REQ_TIMEOUT = 3
+import sys
 
 
 class AsyncRequestHandler:
@@ -12,8 +10,8 @@ class AsyncRequestHandler:
     _results = []
 
     def __init__(self):
-        self._concurrent_requests = 15
-        self._req_timeout = 3
+        self._concurrent_requests = 18
+        self._req_timeout = 10
 
     def load_from_csv(self, csvfile, limit):
         with open(csvfile) as csvfile:
@@ -29,21 +27,56 @@ class AsyncRequestHandler:
         with (yield from semaphore):
             try:
                 furl = 'http://{}/'.format(url)
-                res = yield from asyncio.wait_for(aiohttp.get(furl), REQ_TIMEOUT)
+                res = yield from asyncio.wait_for(aiohttp.get(furl), self._req_timeout)
                 res.close()
                 code = res.status
+
             except:
                 pass
+        self._results.append(code)
         print(str(code) + ":" + url)
+
+    def get_results(self):
+        status_ok = 0
+        total = 0
+        status_none = 0
+        status_other = 0
+
+        for result in self._results:
+            total += 1
+            if result == '000':
+                status_none += 1
+            elif result == 200:
+                status_ok += 1
+            else:
+                status_other += 1
+        print('Total: {}'.format(total))
+        print('200 : {0} : {1}'.format(status_ok, (status_ok / total * 100)))
+        print('000 : {0} : {1}'.format(status_none, (status_none / total * 100)))
 
     def run(self):
         sem = asyncio.Semaphore(self._concurrent_requests)
+
         loop = asyncio.get_event_loop()
 
         coros = []
         for url in self._url_list:
             coros.append(asyncio.async(self._get_status(url, sem)))
 
+        start_time = datetime.datetime.now()
         loop.run_until_complete(asyncio.wait(coros))
         loop._default_executor.shutdown(wait=True)
+        time_elapsed = datetime.datetime.now() - start_time
+        est_time = time_elapsed * 200000 / len(coros)
         loop.close()
+
+        # Print results
+        print('Elapsed: {}'.format(time_elapsed))
+        print('Estimated: {}'.format(est_time))
+        self.get_results()
+
+
+if __name__ == '__main__':
+    handler = AsyncRequestHandler()
+    handler.load_from_csv('alexa.csv', 5000)
+    handler.run()
